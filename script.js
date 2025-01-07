0bfd4e3ccf79d829eea544d009b5d900cecc0f92
KhushiSingh0001 <khushisinghh0001@gmail.com>
2025-01-07 08:56:19 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Placeholder for map functionality
+    const mapContainer = document.getElementById('map');
+    mapContainer.innerHTML = "<p>Interactive map will be here</p>";
+  });
+  
